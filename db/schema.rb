7047962205f8efad4292fd938ff8e2d9a05036a3
John Chambers <johncham81@gmail.com>
2016-06-08 14:58:08 +0100

    removing duplicate migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606120252) do
+ActiveRecord::Schema.define(version: 20160605150510) do
 
   create_table "measurements", force: :cascade do |t|
     t.float    "height"
