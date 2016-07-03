3d050cafead1227d8555c279fd445a5a6cfbcac1
colemai <colemai@tcd.ie>
2016-07-03 12:50:50 +0100

    trouble with req calculation

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -8,7 +8,25 @@ class Measurement < ActiveRecord::Base
   end
 
   def req
-  	req = ()
+  	req =( 
+	  	if gender == "Male"
+		  	case age 
+		  		when 10..18 then ((18.4*weight +581)*pal)
+		  		when 18..30 then ((16.0*weight +545)*pal)
+		  		when 30..60 then ((14.2*weight +593)*pal)
+		  		when 60..70 then ((13.0*weight +567)*pal)
+		  		when 70..120 then ((13.7*weight +481)*pal)
+		  	end
+	  	elsif gender == "Female"
+	  		case age
+	  		    when 10..18 then ((11.1*weight +761)*pal)
+	  		    when 18..30 then ((13.1*weight +558)*pal)
+	  		    when 30..60 then ((9.74*weight +694)*pal)
+	  		    when 60..70 then ((10.2*weight +572)*pal)
+	  		    when 70..120 then ((10.0*weight +577)*pal)
+	  		end
+	)
+
   end
 
    
